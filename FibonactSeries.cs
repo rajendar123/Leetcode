af5afa9e6451500dddcf95fe0ea977c5b7d96da5
Rajendar Manda <42565521+rajendar123@users.noreply.github.com>
2022-04-18 19:36:04 +0530

    Add files via upload

---
+++ FibonactSeries.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int FibonactSeries(int index)
+        {
+            if (index == 0) return 0;
+            if (index == 1) return 1;
+
+            return FibonactSeries(index - 1) + FibonactSeries(index - 2);
+        }
+    }
+}
